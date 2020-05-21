05af863d8b8eecb4fb558966429e639d599ea969
learninto <993232306@qq.cn>
2020-05-21 13:08:48 +0800

    <style><server><抽离通用方法 check_login>

--- cmd/server/http.go
+++ cmd/server/http.go
@@ -2,34 +2,27 @@ package server
 
 import (
 	"kingstar-go/sniper/trace"
-	twirphook "kingstar-go/sniper/twirp-hook"
+	twirphook "kingstar-go/sniper/twirp_hook"
 	"net/http"
 
-	"sniper-api/cmd/server/hook"
-
 	"github.com/bilibili/twirp"
-
-	shop_v1 "sniper-api/rpc/shop/v1"
-	"sniper-api/server/shopserver1"
 )
 
 var hooks = twirp.ChainHooks(
+	twirphook.NewHeaders(),
 	trace.NewRequestID(),
 	twirphook.NewLog(),
 )
 
 var loginHooks = twirp.ChainHooks(
+	twirphook.NewHeaders(),
 	trace.NewRequestID(),
 	twirphook.NewLog(),
-	hook.NewCheckLogin(),
+	twirphook.NewCheckLogin(),
 )
 
 func initMux(mux *http.ServeMux, isInternal bool) {
-	{
-		server := &shopserver1.Server{}
-		handler := shop_v1.NewShopServer(server, hooks)
-		mux.Handle(shop_v1.ShopPathPrefix, handler)
-	}
+
 }
 
 func initInternalMux(mux *http.ServeMux) {
--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.14
 
 require (
 	github.com/bilibili/twirp v0.0.0-20200305140827-a09be7e42ab8
-	github.com/golang/protobuf v1.3.5
+	github.com/golang/protobuf v1.3.5 // indirect
 	github.com/spf13/cobra v0.0.5
+	kingstar-go/commons v0.0.0
 	kingstar-go/sniper v0.0.0
 )
+
+replace kingstar-go/commons => ./../kingstar-go/commons
 
 replace kingstar-go/sniper => ./../kingstar-go/sniper
